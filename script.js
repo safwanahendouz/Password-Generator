22bae7e3b65fca233277fd303fc786b548123c4f
safwanahendouz <safwanahendouz241@gmail.com>
2026-03-07 12:52:50 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let inputSlider = document.getElementById("inputSlider");
 let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let Uppercase = document.getElementById("Uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
 
 // showing input slider value
 sliderValue.textContent = inputSlider.value;
 inputSlider.addEventListener('input', ()=>{
     sliderValue.textContent = inputSlider.value;
 });
+
+genBtn.addEventListener('click', ()=> {
+    passBox.value = generatePassword();
+})
+
+// Function to generate Password
+function generatePassword(){
+    let genPassword = "";
+    genPassword = Math.floor(Math.random() * 50);
+    return genPassword;
+}
